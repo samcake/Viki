5358c9ad07555107af24daa3fbf9d8873d5a845b
Sam <sam@highfidelity.io>
2019-11-18 00:05:02 -0800

    Trying to use VOlk correctly

--- viki.cpp
+++ viki.cpp
@@ -5,7 +5,18 @@
 
 #include <iostream>
 
-#define VK_USE_PLATFORM_MACOS_MVK
+/* Set platform defines at build time for volk to pick up. */
+#if defined(_WIN32)
+#   define VK_USE_PLATFORM_WIN32_KHR
+#elif defined(__linux__) || defined(__unix__)
+#   define VK_USE_PLATFORM_XLIB_KHR
+#elif defined(__APPLE__)
+#   define VK_USE_PLATFORM_MACOS_MVK
+#else
+#   error "Platform not supported by this example."
+#endif
+
+#define VOLK_IMPLEMENTATION
 #include "volk.h"
 //#include <vulkan/vulkan.h>
 
